e9c7f31fdd65207cc42294b485c8cab19463d164
TangJames <Tang.james91@gmail.com>
2020-04-22 12:58:08 -0500

    Removed some code

--- script.py
+++ script.py
@@ -1,20 +1,8 @@
-import math
-import sys
-from os import rename
-
 import requests
-
 # name = input('Your name? ')
 # print("Hello,", name)
-
-print(sys.version)
-print(sys.executable)
-
-
-def greet(who_to_greet):
-    greeting = "Hello, {}".format(who_to_greet)
-    return greeting
 
 
 r = requests.get("http://www.tangjames.com/")
 print(r.status_code)
+print(r.ok)
